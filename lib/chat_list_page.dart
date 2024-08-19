d425156b08aafe5e344efcfa32d4302f9038ea02
disha <dishatimbadiya055@gmail.com>
2024-08-19 21:12:29 +0530

    chatlist and one on one chat modified

--- lib/chat_list_page.dart
+++ lib/chat_list_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ChatListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Static list of contacts
+    
     final List<Map<String, String>> contacts = [
       {'name': 'abc', 'profile': '../../assets/profile1.png'},
       {'name': 'abc', 'profile': '../../assets/profile1.png'},
